2f082d4d8cb750b4c8004c7b87f3b3a5def98dd1
husky <zsigmond.tatrai@gmail.com>
2025-03-14 23:25:27 +0100

    menu frissitve + beallitasok

--- Menu(game_launcher).py
+++ Menu(game_launcher).py
@@ -11,11 +11,11 @@ def menu(esc):
     pygame.display.set_caption("Space War")
 
     play_button = pygame.image.load("Pictures/menu_pictures/playbutton.png").convert_alpha()
-    play_button_coll = pygame.image.load("Pictures/menu_pictures/playbuttoncolliderect.png").convert_alpha()
-    options_button = pygame.image.load("Pictures/menu_pictures/playotions.png").convert_alpha()
-    options_button_coll = pygame.image.load("Pictures/menu_pictures/playoptionscolliderect.png").convert_alpha()
-    exit_button = pygame.image.load("Pictures/menu_pictures/playexit.png").convert_alpha()
-    exit_button_coll = pygame.image.load("Pictures/menu_pictures/playexitcolliderect.png").convert_alpha()
+    options_button = pygame.image.load("Pictures/menu_pictures/optionsbutton.png").convert_alpha()
+    exit_button = pygame.image.load("Pictures/menu_pictures/exitbutton.png").convert_alpha()
+    ship = pygame.image.load("Pictures/players_pictures/player_ship_4_hp_j.png").convert_alpha()
+    options_controls = pygame.image.load("Pictures/menu_pictures/options_controls.png").convert_alpha()
+    back_button = pygame.image.load("Pictures/menu_pictures/backbutton.png").convert_alpha()
 
     running = True
 
@@ -40,17 +40,26 @@ def menu(esc):
             game_quit = menu_screen.blit(exit_button, ((1280 / 2 - 100), 450, 200, 80))
 
             if play_game.colliderect(mouse_form):
-                play_game = menu_screen.blit(play_button_coll, ((1280 / 2 - 142.5), 200, 200, 80))
+                menu_screen.blit(ship,((1280 / 2 - 165), 200))
             if options.colliderect(mouse_form):
-                options = menu_screen.blit(options_button_coll, ((1280 / 2 - 142.5), 325, 200, 80))
+                menu_screen.blit(ship, ((1280 / 2 - 165), 325))
             if game_quit.colliderect(mouse_form):
-                game_quit = menu_screen.blit(exit_button_coll, ((1280 / 2 - 142.5), 450, 200, 80))
+                menu_screen.blit(ship, ((1280 / 2 - 165), 450))
             if play_game.colliderect(mouse_form) and mouse_click:
                 main()
             if options.colliderect(mouse_form) and mouse_click:
                 which_menu = "options"
             if game_quit.colliderect(mouse_form) and mouse_click:
                 running = False
+
+        if which_menu == "options":
+            menu_screen.blit(options_controls, (330, 100))
+            back_main_menu = menu_screen.blit(back_button, (540, 550))
+
+            if back_main_menu.colliderect(mouse_form):
+                menu_screen.blit(ship, ((1280 / 2 - 165), 550))
+            if back_main_menu.colliderect(mouse_form) and mouse_click:
+                which_menu = "main menu"
 
         pygame.display.flip()
         menu_clock.tick(60)
