4d474aa6ec3ba5dba6f3f874a8eb22a252b8260f
husky <zsigmond.tatrai@gmail.com>
2025-03-25 18:44:37 +0100

    boss run-ja nagyabol meg van

--- enemies/first_boss.py
+++ enemies/first_boss.py
@@ -44,25 +44,40 @@ class FirstBoss:
             if self.state == FirstBoss.STATE_INIT:
                 self.y = self.y + 2
                 if self.y >= 0:
-                    self.run_dest = [((context.width - self.width), (context.height - self.height)), ((context.width - self.width), self.height)]
+                    self.run_dest = [((context.width - self.width), (context.height - self.height)), ((context.width - self.width), self.height), (0, (context.height - self.height)), (0, 0)]
                     self.state = FirstBoss.STATE_RUNS
-                    self.dir_x, self.dir_y = get_direction(self.x, self.y, (self.run_dest[0])[0], (self.run_dest[0])[1])
-                    self.start_time = context.time
-                    self.x_0, self.y_0 = self.x, self.y
+                    self.run_dest_reset(0, context)
             if self.state == FirstBoss.STATE_RUNS:
                 d_t = context.time - self.start_time
-                if self.x <= (self.run_dest[self.run_number])[0]:
+                if self.run_number == 0:
                     self.run(d_t)
-                else:
-                    if self.run_number == 0:
-                        self.dir_x, self.dir_y = get_direction(self.x, self.y, (self.run_dest[1])[0], (self.run_dest[1])[1])
-                        self.x_0, self.y_0 = self.x, self.y
+                    if not self.x <= (self.run_dest[self.run_number])[0]:
+                        self.run_dest_reset(1, context)
                         self.run_number = self.run_number + 1
-                        self.start_time = context.time
-
-                if self.run_number == 1 and self.y >= 0:
+                        d_t = context.time - self.start_time
+                if self.run_number == 1 and (self.y <= -500 or self.y >= 0):
                     self.run(d_t)
+                    if self.y <= 0 and self.y >= -10:
+                        self.run_dest_reset(2, context)
+                        self.run_number = self.run_number + 1
+                        d_t = context.time - self.start_time
+                if self.run_number == 2:
+                    self.run(d_t)
+                    if self.x <= 0:
+                        self.run_dest_reset(3, context)
+                        self.run_number = self.run_number + 1
+                        d_t = context.time - self.start_time
+                if self.run_number == 3:
+                    self.run(d_t)
+                    if self.y <= -self.height:
+                        self.state = FirstBoss.STATE_MOVE
+
 
     def run(self, d_t):
         self.x = self.x_0 + self.dir_x * d_t * 500
         self.y = self.y_0 + self.dir_y * d_t * 500
+
+    def run_dest_reset(self, number, context: PygameContext):
+        self.dir_x, self.dir_y = get_direction(self.x, self.y, (self.run_dest[number])[0], (self.run_dest[number])[1])
+        self.start_time = context.time
+        self.x_0, self.y_0 = self.x, self.y
