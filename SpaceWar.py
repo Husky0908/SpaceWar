054b79c6b906d60e0d2e05f1c8d34efc4f735747
husky <zsigmond.tatrai@gmail.com>
2025-04-09 09:05:42 +0200

    game ki classozva

--- SpaceWar.py
+++ SpaceWar.py
@@ -1,14 +1,15 @@
 import pygame
 from base.context import PygameContext
 from menu.menu import menu
-from game.game import game
+from game.game import Game
 from texts.options_save import OptionsSave
 
 
 context = PygameContext(1280, 720)
 options_saving = OptionsSave()
+game = Game()
 
 while not menu(context, options_saving):
-    game(context, options_saving)
+    game.game(context, options_saving)
 
 pygame.quit()
--- game/game.py
+++ game/game.py
@@ -10,127 +10,127 @@ from enemies.first_boss import FirstBoss
 from game.game_logic import GameLogic
 from texts.options_save import OptionsSave
 
+class Game:
 
-def control(context: PygameContext, player: Player, runners: Runners, bullets: Bullets, bullet_shooters: BulletShooters, rocket_launchers: RocketLaunchers, rockets: Rockets, first_boss: FirstBoss):
-    runners.control(context, player)
-    bullet_shooters.control(context, player, bullets)
-    rocket_launchers.control(context, player, rockets)
-    first_boss.control(context, player, bullets)
-    bullets.control(context)
-    rockets.control(player)
-
-
-def contacts(context: PygameContext, player: Player, runners: Runners, bullets: Bullets, bullet_shooters: BulletShooters, rockets: Rockets, rocket_launchers: RocketLaunchers, first_boss: FirstBoss):
-
-    runners.contacts(player, bullets)
-    bullet_shooters.contacts(context, bullets)
-    player.contacts(bullets)
-    first_boss.contacts(bullets, player)
-
-    for bullet in bullets.elements:
-
-        rocket_launchers.contacts(bullet)
-
-        for rocket in rockets.elements:
-            if bullet.form.colliderect(rocket.form) and bullet.attacker == "friend":
-                bullet.sharp = False
-                rocket.health = rocket.health - 1
-
-    rockets.contacts(player)
-    bullets.contacts()
+    def control(self, context: PygameContext, player: Player, runners: Runners, bullets: Bullets, bullet_shooters: BulletShooters, rocket_launchers: RocketLaunchers, rockets: Rockets, first_boss: FirstBoss):
+        runners.control(context, player)
+        bullet_shooters.control(context, player, bullets)
+        rocket_launchers.control(context, player, rockets)
+        first_boss.control(context, player, bullets)
+        bullets.control(context)
+        rockets.control(player)
 
 
-def draw_mouse(context: PygameContext, player: Player):
-    mouse_x, mouse_y = pygame.mouse.get_pos()
-    #pygame.draw.circle(context.screen, (255, 255, 255), (mouse_x, mouse_y), 10)
-    context.screen.blit(player.crosshair_form, (mouse_x, mouse_y))
+    def contacts(self, context: PygameContext, player: Player, runners: Runners, bullets: Bullets, bullet_shooters: BulletShooters, rockets: Rockets, rocket_launchers: RocketLaunchers, first_boss: FirstBoss):
 
-def draw(context: PygameContext, player: Player, runners: Runners, bullets: Bullets, bullet_shooters: BulletShooters, rocket_launchers: RocketLaunchers, rockets: Rockets, first_boss: FirstBoss):
-    context.screen.fill((0, 0, 0))
+        runners.contacts(player, bullets)
+        bullet_shooters.contacts(context, bullets)
+        player.contacts(bullets)
+        first_boss.contacts(bullets, player)
 
-    bullets.draw(context)
-    bullet_shooters.draw(context)
-    runners.draw(context)
-    rocket_launchers.draw(context)
-    rockets.draw(context)
-    first_boss.draw(context)
-    player.draw(context)
-    draw_mouse(context, player)
+        for bullet in bullets.elements:
 
-    pygame.display.flip()
+            rocket_launchers.contacts(bullet)
 
+            for rocket in rockets.elements:
+                if bullet.form.colliderect(rocket.form) and bullet.attacker == "friend":
+                    bullet.sharp = False
+                    rocket.health = rocket.health - 1
 
-def get_char(event: pygame.event):
-    if event.type == pygame.KEYUP:
-        return event.key
-    
+        rockets.contacts(player)
+        bullets.contacts()
 
 
-def game(context: PygameContext, options_saving: OptionsSave):
-    player = Player(context.width // 2, context.height // 2)
-    first_boss = FirstBoss(0, -350)
+    def draw_mouse(self, context: PygameContext, player: Player):
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        context.screen.blit(player.crosshair_form, (mouse_x, mouse_y))
 
-    game_logic_parameters = GameLogic(options_saving.game_difficulty)
+    def draw(self, context: PygameContext, player: Player, runners: Runners, bullets: Bullets, bullet_shooters: BulletShooters, rocket_launchers: RocketLaunchers, rockets: Rockets, first_boss: FirstBoss):
+        context.screen.fill((0, 0, 0))
 
-    bullets = Bullets()
-    rockets = Rockets()
-    runners = Runners()
-    bullet_shooters = BulletShooters()
-    rocket_launchers = RocketLaunchers()
+        bullets.draw(context)
+        bullet_shooters.draw(context)
+        runners.draw(context)
+        rocket_launchers.draw(context)
+        rockets.draw(context)
+        first_boss.draw(context)
+        player.draw(context)
+        self.draw_mouse(context, player)
 
-    running = True
-    cheat_mode = False
-    input_timeout = 0
+        pygame.display.flip()
 
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                player.press_mouse(running, bullets, context)
 
-            if cheat_mode and input_timeout == 0:
-                c = get_char(event)
-                if c == pygame.K_ESCAPE:
+    def get_char(self, event: pygame.event):
+        if event.type == pygame.KEYUP:
+            return event.key
+
+
+
+    def game(self, context: PygameContext, options_saving: OptionsSave):
+        player = Player(context.width // 2, context.height // 2)
+        first_boss = FirstBoss(0, -350)
+
+        game_logic_parameters = GameLogic(options_saving.game_difficulty)
+
+        bullets = Bullets()
+        rockets = Rockets()
+        runners = Runners()
+        bullet_shooters = BulletShooters()
+        rocket_launchers = RocketLaunchers()
+
+        running = True
+        cheat_mode = False
+        input_timeout = 0
+
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    player.press_mouse(running, bullets, context)
+
+                if cheat_mode and input_timeout == 0:
+                    c = self.get_char(event)
+                    if c == pygame.K_ESCAPE:
+                        cheat_mode = False
+                    if c is not None:
+                        cheat = cheat + chr(c)
+
+
+                if not cheat_mode and event.type == pygame.KEYUP and event.unicode == chr(pygame.K_m):
+                    cheat_mode = True
+                    cheat = ""
+
+            #if keys[pygame.K_ESCAPE]:
+            #    menu(True)
+
+
+            if not cheat_mode:
+                context.time = context.time + context.delta_time
+
+                running = player.control(context, running)
+                self.control(context, player, runners, bullets, bullet_shooters, rocket_launchers, rockets, first_boss)
+                self.draw(context, player, runners, bullets, bullet_shooters, rocket_launchers, rockets, first_boss)
+                self.contacts(context, player, runners, bullets, bullet_shooters, rockets, rocket_launchers, first_boss)
+                game_logic_parameters.wave_logic(context, bullet_shooters, runners, rocket_launchers, first_boss)
+            else:
+                if cheat == "megh":
+                    player.health = 100
                     cheat_mode = False
-                if c is not None:
-                    cheat = cheat + chr(c)
+                if cheat == "maxh":
+                    player.health = 5
+                    cheat_mode = False
+                if cheat == "spo1b":
+                    bullets.elements.clear()
+                    rockets.elements.clear()
+                    runners.elements.clear()
+                    bullet_shooters._elements.clear()
+                    rocket_launchers.elements.clear()
+
+                    game_logic_parameters.wave_time = 16000
+                    game_logic_parameters.wave = 8
+                    cheat_mode = False
 
 
-            if not cheat_mode and event.type == pygame.KEYUP and event.unicode == chr(pygame.K_m):
-                cheat_mode = True
-                cheat = ""
-                
-        #if keys[pygame.K_ESCAPE]:
-        #    menu(True)
-        
-
-        if not cheat_mode:
-            context.time = context.time + context.delta_time
-            
-            running = player.control(context, running)
-            control(context, player, runners, bullets, bullet_shooters, rocket_launchers, rockets, first_boss)
-            draw(context, player, runners, bullets, bullet_shooters, rocket_launchers, rockets, first_boss)
-            contacts(context, player, runners, bullets, bullet_shooters, rockets, rocket_launchers, first_boss)
-            game_logic_parameters.wave_logic(context, bullet_shooters, runners, rocket_launchers, first_boss)
-        else:
-            if cheat == "megh":
-                player.health = 100
-                cheat_mode = False
-            if cheat == "maxh":
-                player.health = 5
-                cheat_mode = False
-            if cheat == "spo1b":
-                bullets.elements.clear()
-                rockets.elements.clear()
-                runners.elements.clear()
-                bullet_shooters._elements.clear()
-                rocket_launchers.elements.clear()
-                
-                game_logic_parameters.wave_time = 16000
-                game_logic_parameters.wave = 8
-                cheat_mode = False
-
-
-        context.delta_time = context.clock.tick(60) / 1000
+            context.delta_time = context.clock.tick(60) / 1000
 
